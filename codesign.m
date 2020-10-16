e81e91f9e9966fc5a73ffcdd8070a34402ce0012
mtv3673crawlab <minhvu0194@gmail.com>
2020-10-16 12:31:56 -0500

    Codesign from start without random sampling
    Make the code neat and ready to run.

--- codesign.m
+++ codesign.m
@@ -1,185 +1,9 @@
-clear
-clc
-close all
-
-
-syms xi1 xi2 xi3 xi4 xi5 dt
-
-
-% furuta
-res = furuta('model');
-F= [xi3;
-    xi4;
-    res(1)
-    res(2)
-    0];
-F = simplify(F);
-
-p = 4;
-
-% [Phi,Psi_p,JPhi] = compute_Phi_and_JPhi(p,F,[xi1 xi2 xi3 xi4 xi5],dt);
-[Phi_real,Psi_p_real,JPhi_real] = compute_Phi_and_JPhi(p,F,[xi1 xi2 xi3 xi4 xi5],dt);
-
-
-disp( 'Done!' );
-save('furuta_real.mat','Phi_real','Psi_p_real','JPhi_real')
-
-
-%% Pre-processing: colect 5 small random trajectories around x0  
+%% Part 1: Apply u to the system --> collect data --> train NN --> compute u --> repeat 
 clear
 clc
 close all
 load('dis_info.mat')
 
-x_start_sample = [0 pi 0 0]';
-
-dt = 0.01;
-T = 2;
-iter_max = ceil(T/dt);
-
-tt = dt:dt:T;
-
-signal_store = [ones(iter_max,1), sin(2*tt)', cos(2*tt)', sin(4*tt)', cos(4*tt)', sin(6*tt)', cos(6*tt)', sin(8*tt)', cos(8*tt)', sin(10*tt)', cos(10*tt)',sin(12*tt)', cos(12*tt)'];
-nbasis = size(signal_store,2);
-
-NN_input = [];
-NN_output = [];
-for k_sample = 1:5
-                
-        alpha = 0.3*k_sample*(rand(nbasis,1)-.5)*2;              
-        u_test = signal_store*alpha;        
-%         [max(u_test), min(u_test)]       
-                   
-        x0 = x_start_sample + 0.1*(rand(length(x_start_sample),1)-.5)*2;
-        x = x0;
-        x_traj = x0;
-        
-        % real system
-        for iter = 1:size(u_test,1)            
-            [~, x_trajJ_fine] = adaptive_taylor(p,Phi,Psi_p,[0 dt],[x;u_test(iter)]);
-            x = x_trajJ_fine(end,:)';
-            x = x(1:4);
-            x_traj = [x_traj x];
-        end
-
-        x_traj(2,:) = sign(x_traj(2,:)).*mod(abs(x_traj(2,:)),2*pi);
-
-        NN_input = [NN_input, [x_traj(2:4,1:end-1);u_test'] ];
-        NN_output = [NN_output, x_traj(3:4,2:end)];
-        
-        % Use the symmetry of the Furuta pendulum 
-%         NN_input = [NN_input, [x_traj(2:4,1:end-1);u_test'], -[x_traj(2:4,1:end-1);u_test']];
-%         NN_output = [NN_output, [x_traj(3,2:end), -x_traj(3,2:end);
-%                                  x_traj(4,2:end), -x_traj(4,2:end)] ];
-
-        % remove data that are too large (outside the neccessary area)         
-        FI = find((-pi <=  NN_input(1,:)));
-        NN_input = NN_input(:,FI);
-        NN_output = NN_output(:,FI);
-        
-        FI = find(abs(NN_input(4,:))<=10);
-        NN_input = NN_input(:,FI);
-        NN_output = NN_output(:,FI);
-        
-        clf
-        plot3(NN_input(1,:),NN_input(3,:),NN_input(4,:),'.');
-        view(3);
-        xlabel('theta 2')
-        ylabel('theta 2 dot')
-        zlabel('u')
-        axis([ -10 10 -30 30 -20 20]);
-
-        drawnow;
-end
-    
-rotate3d on
-
-%% Train NN
-trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
-
-% Create a Fitting Network
-hiddenLayerSize = [20 20];
-net = fitnet(hiddenLayerSize,trainFcn);
-
-% Choose Input and Output Pre/Post-Processing Functions
-% For a list of all processing functions type: help nnprocess
-net.input.processFcns = {'removeconstantrows','mapminmax'};
-net.output.processFcns = {'removeconstantrows','mapminmax'};
-
-% Setup Division of Data for Training, Validation, Testing
-% For a list of all data division functions type: help nndivision
-net.divideFcn = 'dividerand';  % Divide data randomly
-net.divideMode = 'sample';  % Divide up every sample
-net.divideParam.trainRatio = 85/100;
-net.divideParam.valRatio = 10/100;
-net.divideParam.testRatio = 5/100;
-
-net.trainParam.epochs=400;
-
-% Choose a Performance Function
-% For a list of all performance functions type: help nnperformance
-net.performFcn = 'mse';  % Mean Squared Error
-
-% Train the Network
-[net,tr] = train(net,NN_input,NN_output,'useParallel','yes','showResources','yes');
-% view(net)
-genFunction(net,'myNN');
-
-% Automatically replace [Y,Xf,Af] with [Y,Xf,Af,x1_step1,y1_step1]
-A = regexp( fileread('myNN.m'), '\n', 'split');
-A{1} = sprintf('function [Y,Xf,Af,x1_step1,y1_step1] = %s(X,~,~)','myNN');
-fid = fopen('myNN.m', 'w');
-fprintf(fid, '%s\n', A{:});
-fclose('all');
-
-[~,~,~,x1_step1,y1_step1] = myNN(rand(4,1));
-
-%% test how well NN can capture the local dynamics 
-x0 = [0;pi;0;0];
-iter_max = 50;
-u = 0.1*ones(iter_max,1);
-x = x0;
-x_traj = [];
-[~,~,~,x1_step1,y1_step1] = myNN(rand(4,1));
-
-for iter = 1:iter_max
-    % Prepare A,B to later calculate H
-    R_big = JPhi(dt,x(1),x(2),x(3),x(4),u(iter));
-    A_real_store{iter} = R_big(1:4,1:4);     % A in Ax+Bu
-    B_real_store{iter} = R_big(1:4,5:5);     % B in Ax+Bu
-    
-    jac = NN_jacob(net, [x(2:4);u(iter)], x1_step1, y1_step1);
-    A_store{iter} = [1    0          dt          0;
-                     0    1          0           dt;
-                     0 jac(1,1)  jac(1,2)  jac(1,3);
-                     0 jac(2,1)  jac(2,2)  jac(2,3);];
-    B_store{iter} = [0;
-                     0;
-                     jac(1,4);
-                     jac(2,4)];
-    
-    % The actual trajectory using adaptive step size mechanism
-    [~, x_trajJ_fine] = adaptive_taylor(p,Phi,Psi_p,[0 dt],[x;u(iter)]);
-    x = x_trajJ_fine(end,:)';
-    x = x(1:4);
-    x_traj = [x_traj, x];
-end
-
-% Calculate H:
-H = B_store{1};
-for iter = 2:iter_max
-    H = [A_store{iter}*H, B_store{iter}];
-end
-    
-H_real = B_real_store{1};
-for iter = 2:iter_max
-    H_real = [A_real_store{iter}*H_real, B_real_store{iter}];
-end
-
-RMSE = sqrt(mean((H(:)-H_real(:)).^2))  
-
-%% Part 1: Apply u to the system --> collect data --> train NN --> compute u --> repeat 
-clc
 dt = 0.01;
 T = 2;
 iter_max = ceil(T/dt);
@@ -187,9 +11,10 @@ iter_max = ceil(T/dt);
 x0 = [0;pi;0;0];
 x_target = [0;0;0;0];
 
-u = 0.1*ones(iter_max,1);
-% u = 0.1*rand(iter_max,1);
+% u = 0.1*ones(iter_max,1);
+u = 0.5*rand(iter_max,1);
 
+net = []; NN_input = []; NN_output = [];
 rep = 1;
 count = 0;
 rejected = false;
@@ -211,7 +36,7 @@ while continue_iterating  % Iterating scheme: U --> trajactory [x(0)...x(N)] -->
         
         % train NN 
         [NN_input,NN_output] = store_training_data(NN_input,NN_output,x_traj,u); 
-        [net,x1_step1,y1_step1,NN_input,NN_output] = sampling_and_train(x_start_sample,net,NN_input,NN_output);
+        [net,x1_step1,y1_step1,NN_input,NN_output] = sampling_and_train(x0,net,NN_input,NN_output);
         
         for iter = 1:iter_max  
             % Prepare A,B to later calculate H        
@@ -307,15 +132,6 @@ dt*norm(u)^2
 step1_u = u;
 disp( 'Done1!' );
 Minh_Animation(x_traj)
-%%
-% Minh_Animation(x_traj)
-figure
-% clf
-% hold on
-% plot3(NN_input(1,:),NN_input(3,:),NN_input(4,:),'.');
-% mmm = plot3(x_traj(2,2:end),x_traj(4,2:end),u,'r');
-% mmm2 = scatter3(x_start_sample(2),x_start_sample(4),0,100,'r')
-plot(store_data(1,:))
 
 %% Minh - Animation:
 
@@ -356,7 +172,6 @@ end
 rotate3d on
 
 end
-
 
 function [net,x1_step1,y1_step1,NN_input,NN_output] = sampling_and_train(x_start_sample,net,NN_input,NN_output)
 % -------------------- sampling --------------------
@@ -403,18 +218,36 @@ end
 
 
 % -------------------- training --------------------
-net.trainParam.epochs=250;
+if isempty(net)
+    trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
+    hiddenLayerSize = [20 20];
+    net = fitnet(hiddenLayerSize,trainFcn);
 
-% Train the Network
-[net,tr] = train(net,NN_input,NN_output,'useParallel','yes','showResources','no');
+    net.input.processFcns = {'removeconstantrows','mapminmax'};
+    net.output.processFcns = {'removeconstantrows','mapminmax'};
 
-% Retrain if needed
-retrain = 0;
-while strcmp(tr.stop,'Validation stop.') && tr.num_epochs<100 && retrain<3
-%     display('retrain due to bad training!')
-    net.trainParam.epochs=400;
+    net.divideFcn = 'dividerand';  % Divide data randomly
+    net.divideMode = 'sample';  % Divide up every sample
+    net.divideParam.trainRatio = 85/100;
+    net.divideParam.valRatio = 10/100;
+    net.divideParam.testRatio = 5/100;
+
+    net.trainParam.epochs=250;
+    net.performFcn = 'mse';  % Mean Squared Error
+
+    [net,tr] = train(net,NN_input,NN_output,'useParallel','yes','showResources','yes');
+else
+    net.trainParam.epochs=250;
+    % Train the Network
     [net,tr] = train(net,NN_input,NN_output,'useParallel','yes','showResources','no');
-    retrain = retrain + 1;
+    % Retrain if needed
+    retrain = 0;
+    while strcmp(tr.stop,'Validation stop.') && tr.num_epochs<100 && retrain<3
+    %     display('retrain due to bad training!')
+        net.trainParam.epochs=400;
+        [net,tr] = train(net,NN_input,NN_output,'useParallel','yes','showResources','no');
+        retrain = retrain + 1;
+    end
 end
 
 % Save the NN 
